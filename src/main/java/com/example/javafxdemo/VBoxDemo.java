f7e80c2aee4d975805f5f39565064495b7d11a1c
Kritik Nayaju <kirtiknayaju@gmail.com>
2023-08-09 19:05:38 +0545

    gridpane flowpane hbox and vbox framework is done

--- src/main/java/com/example/javafxdemo/VBoxDemo.java
+++ src/main/java/com/example/javafxdemo/VBoxDemo.java
@@ -1,6 +1,8 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +17,15 @@ public class VBoxDemo extends Application {
     public void start(Stage stage) throws Exception {
         VBox vBox = new VBox();
 
-        for (int i=1; )
+        for (int i=1; i<=5; i++){
+            Button button = new Button("Button" + i);
+            vBox.getChildren().add(button);
+        }
+
+
+        Scene scene = new Scene(vBox, 600, 400);
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
